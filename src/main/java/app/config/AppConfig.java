245e8883816371bddc2ef1c5a3daf36338226bbb
KatalonecSpb <79518241+KatalonecSpb@users.noreply.github.com>
2023-01-15 13:09:21 +0300

    Update AppConfig.java

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,7 @@
 package app.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
 @ComponentScan(basePackages = "app")
